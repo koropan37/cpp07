ca6029ec182ab5e5958e7752897b8ac1911b774e
koro <shunta37k@gmail.com>
2025-12-12 20:53:28 +0900

    12/12

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,6 +2,81 @@
 #include "Array.hpp"
 
 #define MAX_VAL 750
+
+#define BLACK   "\033[0;30m"
+#define RED     "\033[0;31m"
+#define GREEN   "\033[0;32m"
+#define YELLOW  "\033[0;33m"
+#define BLUE    "\033[0;34m"
+#define MAGENTA "\033[0;35m"
+#define CYAN    "\033[0;36m"
+#define WHITE   "\033[0;37m"
+#define RESET   "\033[0m"
+#define B_RED	"\033[1;31m"
+#define B_GREEN "\033[1;32m"
+#define B_CYAN  "\033[1;36m"
+
+void printColor(std::string const& str, std::string const& color) {
+    std::cout
+	<< RESET << "======="
+	<< color << str
+	<< RESET << "=======" << color << std::endl;
+}
+
+#include <cstdio>
+
+void test(void){
+    printColor("Int", CYAN);
+    {
+        Array<int> a1;
+        Array<int> a2(3);
+        Array<int> a3(a2);
+        Array<int> a4(0);
+        std::cout << "a1.size() = " << a1.size() << std::endl;
+        std::cout << "a2.size() = " << a2.size() << std::endl;
+        std::cout << "a3.size() = " << a3.size() << std::endl;
+        for (unsigned int i = 0; i < a2.size(); i++) {
+        a2[i] = i;
+        std::cout << "a2[" << i << "] = " << a2[i] << std::endl;
+        }
+            for (unsigned int i = 0; i < a3.size(); i++) {
+            a3[i] = i + 10;
+            std::cout << "a3[" << i << "] = " << a3[i] << std::endl;
+        }
+        {
+            const Array<int> a5 = a2;
+            for (unsigned int i = 0; i < a5.size(); i++) {
+            std::cout << "a5[" << i << "] = " << a5[i] << std::endl;
+            }
+        }
+    }
+    {
+        printColor("Std::string", MAGENTA);
+        Array<std::string> a1;
+        Array<std::string> a2(5);
+        Array<std::string> a3(a2);
+        Array<std::string> a4(0);
+        for (unsigned int i = 0; i < a2.size(); i++) {
+            char buf[100];
+            sprintf(buf, "hello %u", i);
+            a2[i] = std::string(buf);
+            std::cout << "a2[" << i << "] = " << a2[i] << std::endl;
+        }
+        for (unsigned int i = 0; i < a3.size(); i++) {
+            char buf[100];
+            sprintf(buf, "world %u", i + 10);
+            a3[i] = std::string(buf);
+            std::cout << "a3[" << i << "] = " << a3[i] << std::endl;
+        }
+        {
+        const Array<std::string> a5 = a2;
+        for (unsigned int i = 0; i < a5.size(); i++) {
+            std::cout << "a5[" << i << "] = " << a5[i] << std::endl;
+        }
+        }
+    }
+}
+
 int main(int, char**)
 {
     Array<int> numbers(MAX_VAL);
@@ -49,5 +124,6 @@ int main(int, char**)
         numbers[i] = rand();
     }
     delete [] mirror;//
+    test();
     return 0;
 }
